94fb355bbe8fc44ca07e8e6c142682fbb7300835
ivn <ivn@localhost.localdomain>
2022-12-18 21:53:50 -0300

    version react proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,68 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 import {
   getAllPokemons,
   getPokemonsWithoutEvolutionChain,
   getPokemonLastEvolution,
-} from './services/poke.api';
-import { PokeList } from './components/PokeList';
-import { PokeListItem } from './components/PokelistItem';
+  getMinWeightLastEvolution,
+} from "./services/poke.api";
+import { PokeList } from "./components/PokeList";
+import { PokeListItem } from "./components/PokelistItem";
 
 function App() {
   const [pokemons, setPokemons] = useState([]);
   const [pokemonWithoutEvolution, setpokemonWithoutEvolution] = useState([]);
   const [pokemonsLastEvolution, setPokemonsLastEvolution] = useState([]);
+  const [pokemonPropertys, setPokemonsPropertys] = useState([]);
+  const [minPokemonWeight, setMinPokemonWeight] = useState(500);
+  const [minPokeWeightName, setMinPokeWeightName] = useState("");
+
+  const setNameAndWeight = () => {
+    pokemonPropertys.map((poke) => {
+      console.log(poke);
+    });
+    console.log("nombre:" + { minPokeWeightName });
+    console.log("peso:" + { minPokemonWeight });
+  };
 
   useEffect(() => {
     const fetchPokemons = async () => {
+      const auxPokemonsLastEvolution = await getPokemonLastEvolution();
       const auxPokemonWithoutEvolution =
         await getPokemonsWithoutEvolutionChain();
       const auxPokemons = await getAllPokemons();
-      const auxPokemonsLastEvolution = await getPokemonLastEvolution();
+      const auxPokemonsPropertys = await getMinWeightLastEvolution();
 
       setPokemons(auxPokemons);
       setpokemonWithoutEvolution(auxPokemonWithoutEvolution);
       setPokemonsLastEvolution(auxPokemonsLastEvolution);
+      setPokemonsPropertys(auxPokemonsPropertys);
     };
-
     fetchPokemons();
-  }, []);
+    setNameAndWeight();
+  });
 
   return (
     <div className="App">
       <h1>hola</h1>
       <div className="resumenPokemons">
-        <h3>Pokemones sin evolucion: {pokemonWithoutEvolution.length}</h3>
+        <h3>Pokemones sin evolucion: {pokemonsLastEvolution.length}</h3>
         <h3>Total de pokemones: {pokemons.length}</h3>
       </div>
+
       <div className="pokemonList">
         <PokeList>
+          <p>pokemon sin evolucion</p>
           {pokemonWithoutEvolution.map((poke, index) => {
             return <PokeListItem key={index} url="" name={poke} />;
           })}
+          {}
         </PokeList>
+
         <PokeList>
-
-          {pokemonsLastEvolution.map(poke)=>{
-            return <PokeListItem key={poke} name={poke}/>
-
+          <p>pokemon last evolucion</p>
+          {pokemonsLastEvolution.map((poke) => {
+            return <PokeListItem key={poke} name={poke}></PokeListItem>;
           })}
         </PokeList>
       </div>
--- src/services/poke.api.jsx
+++ src/services/poke.api.jsx
@@ -1,10 +1,13 @@
-import axios from 'axios';
+import axios from "axios";
 
-const MAIN_URL = 'https://pokeapi.co/api/v2/';
-const ENDPOINT_POKEMON = MAIN_URL + 'pokemon?limit=1160&offset=0';
-const ENDPOINT_EVOLUTIONTRIGGER = MAIN_URL + 'evolution-trigger/';
+const MAIN_URL = "https://pokeapi.co/api/v2/";
+const ENDPOINT_POKEMON = MAIN_URL + "pokemon?limit=1160&offset=0";
+const ENDPOINT_EVOLUTIONTRIGGER = MAIN_URL + "evolution-trigger/";
 const ENDPOINT_EVOLUTIONCHAIN =
-  MAIN_URL + 'evolution-chain?limit=1500&offset=0/';
+  MAIN_URL + "evolution-chain?limit=1500&offset=0/";
+
+let POKENAME = "";
+let pokemonLastEvolution = [];
 
 const getAllPokemons = async () => {
   const response = await axios.get(ENDPOINT_POKEMON);
@@ -14,28 +17,40 @@ const getAllPokemons = async () => {
   }
 };
 
-const getPokemonLastEvolution = async () => {
-  let pokemonLastEvolution = [];
+const getMinWeightLastEvolution = async () => {
+  let pokemonPropertys = [];
+  pokemonLastEvolution.map(async (pokemon) => {
+    const response = await axios.get(MAIN_URL + "pokemon/" + pokemon);
+    const { data } = response;
+    pokemonPropertys.push(data);
+  });
+  return pokemonPropertys;
+};
 
+const getPokemonLastEvolution = async () => {
   const response = await axios.get(ENDPOINT_EVOLUTIONCHAIN);
   const { data, status } = response;
+
   if (status === 200) {
     data.results.forEach(async ({ url }) => {
       const r = await axios.get(url);
       const { data } = r;
       const firstChain = data.chain;
-      pokemonLastEvolution.push(checkEvolveTo(firstChain, ''));
+      await checkEvolveTo(firstChain);
+      pokemonLastEvolution.push(POKENAME);
     });
   }
+
   return pokemonLastEvolution;
 };
 
 async function checkEvolveTo(chain) {
-  if (chain.hasOwnProperty('evolves_to') && chain.evolves_to.length == 0) {
-    return chain.species.name;
-  } else if (chain.hasOwnProperty('evolves_to')) {
+  if (chain.hasOwnProperty("evolves_to") && chain.evolves_to.length == 0) {
+    POKENAME = chain.species.name;
+  } else if (chain.hasOwnProperty("evolves_to")) {
     const newChain = chain.evolves_to[0];
-    checkEvolveTo(newChain);
+
+    await checkEvolveTo(newChain);
   }
 }
 
@@ -65,4 +80,5 @@ export {
   getAllPokemons,
   getPokemonsWithoutEvolutionChain,
   getPokemonLastEvolution,
+  getMinWeightLastEvolution,
 };
